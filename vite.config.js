f9ebb4703f5d3a315c170fef41129b097799dd61
aGaoC <710102012@qq.com>
2025-03-11 16:47:24 +0800

    fix: update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/nexttoken-website/' : '/',
+  base: '/nexttoken-website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
