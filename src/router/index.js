c9d39c5fa842ed10869d943f87af28570182d902
aGaoC <710102012@qq.com>
2025-03-11 17:18:24 +0800

    fix: switch to hash mode for routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 // 导入组件
 import Home from '../views/Home.vue'
@@ -54,7 +54,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
